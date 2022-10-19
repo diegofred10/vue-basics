0f2f489b34f19d2ed640c51347dee96f6dad0695
pilpod <giacomo.corso@gmail.com>
2022-10-19 17:09:22 +0200

    input password reseted

--- src/components/InputPassword.vue
+++ src/components/InputPassword.vue
@@ -1,24 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 
-let inputType = ref('password')
-
-function showPassword() {
-    if (inputType.value == 'password') {
-        inputType.value = 'text'
-        return
-    }
-
-    inputType.value = 'password'
-}
 </script>
 
 <template>
     <div class="container text-center input-password">
         <h1 class="my-3">Exercise Input Password</h1>
-        <input :type="inputType" name="" id="password" class="form-control">
+        <input type="password" name="" id="password" class="form-control">
         <div class="d-block">
-            <span id="show-password" @click="showPassword" class="mt-2 btn btn-primary">Show Password</span>
+            <span id="show-password" class="mt-2 btn btn-primary">Show Password</span>
         </div>
     </div>
 </template>
