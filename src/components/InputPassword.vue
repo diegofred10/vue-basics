27c2879c5b4ada323fbeb59b3f3c2b78d9ae1b11
pilpod <giacomo.corso@gmail.com>
2022-10-19 17:05:00 +0200

    input password done with test

--- src/components/InputPassword.vue
+++ src/components/InputPassword.vue
@@ -1,13 +1,31 @@
 <script setup>
+import { ref } from 'vue';
 
+let inputType = ref('password')
+
+function showPassword() {
+    if (inputType.value == 'password') {
+        inputType.value = 'text'
+        return
+    }
+
+    inputType.value = 'password'
+}
 </script>
 
 <template>
-    <div class="input-password">
-        <h1>Exercise Input Password</h1>
+    <div class="container text-center input-password">
+        <h1 class="my-3">Exercise Input Password</h1>
+        <input :type="inputType" name="" id="password" class="form-control">
+        <div class="d-block">
+            <span id="show-password" @click="showPassword" class="mt-2 btn btn-primary">Show Password</span>
+        </div>
     </div>
 </template>
 
-<style lang="css">
-    
+<style lang="css" scoped>
+    #show-password:hover {
+        cursor: pointer;
+        user-select: none;
+    }
 </style>
