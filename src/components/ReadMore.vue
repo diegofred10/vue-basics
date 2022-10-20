939649b37c160b5523b1c5889eba0403c4915bd1
pilpod <giacomo.corso@gmail.com>
2022-10-20 20:34:15 +0200

    readmore component reseted

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -2,30 +2,14 @@
 import { ref } from "vue";
 
 const title = ref("Exercise Read More");
-const textBtn = ref("Show more");
-const cutTextStyle = ref("cut-text");
 
-function showText() {
-  if (cutTextStyle.value == "") {
-    hideText();
-    return
-  }
-
-  cutTextStyle.value = "";
-  textBtn.value = "Show less";
-}
-
-function hideText() {
-  cutTextStyle.value = "cut-text";
-  textBtn.value = "Show More";
-}
 </script>
 
 <template>
   <div class="container text-center">
     <h1 class="my-3">{{ title }}</h1>
     <div>
-      <p :class="['mb-3', 'd-block', cutTextStyle]">
+      <p class="mb-3 d-block cut-text">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
         ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
         molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
@@ -33,7 +17,7 @@ function hideText() {
         temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
         quaerat autem non hic, quidem dolore laboriosam.
       </p>
-      <button class="btn btn-primary" @click="showText">{{ textBtn }}</button>
+      <button class="btn btn-primary">Show More</button>
     </div>
     <hr class="mb-4" />
   </div>
