6fbd35896005abda09d5abb1cd64152b17cbaaaa
pilpod <giacomo.corso@gmail.com>
2022-10-20 19:30:20 +0200

    readmore template section done

--- README.md
+++ README.md
@@ -38,4 +38,4 @@ npm run test:unit
 
 1. Configura el componente 'InputPassword' para que se muestre la contraseña introducida en el input haciendo click en el botón "show password".
 
-2. Configura el componente 'ReadMore' para que se pueda mostrar el texto completo cuando el usuario hace click en el botón 'Show more'. Cuando el texto este desplegado el botón tiene que mostrar 'Show less'. El parágrafo está utilizando la class 'cut-text' disponible en main.css.
+2. Configura el componente 'ReadMore' para que se pueda mostrar el texto completo cuando el usuario hace click en el botón 'Show more'. Cuando el texto este desplegado el botón tiene que mostrar 'Show less'. El parágrafo está utilizando la class 'cut-text' disponible en el archivo src/asserts/readmore.css.
--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -1,13 +1,26 @@
 <script setup>
+import { ref } from 'vue';
+
+const title = ref('Exercise Read More')
 
 </script>
 
 <template>
+  <div class="container text-center">
+    <h1 class="my-3">{{ title }}</h1>
     <div>
-        <h1>Exercise Read More</h1>
+      <p class="mb-3 d-block cut-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
+        molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
+        deserunt aperiam maxime consectetur recusandae quasi commodi dicta
+        temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
+        quaerat autem non hic, quidem dolore laboriosam.
+      </p>
+      <button class="btn btn-primary">Show more</button>
     </div>
+    <hr class="mb-4" />
+  </div>
 </template>
 
-<style lang="css">
-    
-</style>
+<style lang="css"></style>
